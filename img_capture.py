20fe3023a287280aa1dcd78f647721d8f496f5fb
ubrandrew <uberandrew126@gmail.com>
2018-12-16 22:01:51 -0500

    comments

--- img_capture.py
+++ img_capture.py
@@ -2,9 +2,13 @@ import numpy as np
 import cv2
 from PIL import ImageGrab
 
+#takes screenshot
 img = ImageGrab.grab()
+
+#converts to np array for processing with opencv
 img_np = np.array(img)
 
+#display image
 cv2.imshow("frame", img_np)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
